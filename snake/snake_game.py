28dfc83f09f071abf9916e461980dc43dc42ecfc
Dlovan <dlo.sy@outlook.com>
2026-03-11 02:29:58 +0100

    feat: add keyboard movement for player square

--- snake/snake_game.py
+++ snake/snake_game.py
@@ -9,12 +9,32 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake")
 
 running = True
+# velocity variables
+x = 100
+y = 100
+dx = 0
+dy = 0
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_RIGHT:
+                dx = 5
+                dy = 0
+            if event.type == pygame.K_LEFT:
+                dx = -5
+                dy = 0
+            if event.type == pygame.K_UP:
+                dx = 0
+                dy = -5
+            if event.type == pygame.K_DOWN:
+                dx = 0
+                dy = 5
+    x += dx
+    y += dy
 
     screen.fill((0, 0, 0))
     # add the object to the blanc screen with RGB and positioning
